00331b355bbc1cc2bcfa6417fa49ef9f221906d9
PhilipOyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2021-06-14 00:41:38 +0100

    Add files via upload

--- static/javascript/supplement.js
+++ static/javascript/supplement.js
@@ -2,23 +2,10 @@ function clicked() {
     document.getElementById("content").classList.toggle("show")
 }
 
-var acimages = ["/static/images/air condition installation (indoor-violent).jpg", "/static/images/air condition installation (outdoor) 2.jpg",
-    "/static/images/inverter.jpg", "/static/images/cell.jpg"];
-var acpos = 0;
-var acslider = document.getElementById("acslider");
-var auto = setInterval(acslide, 2000);
-function acslide() {
-    acpos++;
-    if (acpos >= acimages.length) { acpos = 0; }
-    acslider.src = acimages[acpos];
-}
+document.querySelector('.popup').addEventListener('click', function() {
+	document.querySelector('.form-card').classList.add('view');
+});
 
-var swimages = ["/static/images/mlog.png", "/static/images/bmi.png", "/static/images/programmer.jpg", "/static/images/web.png"];
-var swpos = 0;
-var swslider = document.getElementById("swslider");
-var auto = setInterval(swslide, 2000);
-function swslide() {
-    swpos++;
-    if (swpos >= swimages.length) { swpos = 0; }
-    swslider.src = swimages[swpos];
-}
+document.querySelector('.close-btn').addEventListener('click', function() {
+	document.querySelector('.form-card').classList.remove('view');
+});
